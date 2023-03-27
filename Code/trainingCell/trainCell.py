f3d013b193a12a075b4415311d4d9c1d44a20ee2
Esther Vera <esthervera99@gmail.com>
2023-03-27 11:36:26 +0200

    Modificación comentarios, rutas trainCell y otros archivos, ya funciona entrenamiento #6

--- Code/trainingCell/trainCell.py
+++ Code/trainingCell/trainCell.py
@@ -8,7 +8,6 @@ import subprocess
 
 
 
-
 # Clase para el entrenamiento de YOLO para detectar células en la imagen
 ##############################################################
 class trainCell:
@@ -16,37 +15,36 @@ class trainCell:
     def __init__(self, path_dataset):
         self.path_dataset = path_dataset
         self.path_model = ""
+        self.imgs = []
+        self.labels = []
         self.X_train = []
         self.X_val = []
         self.y_train = []
         self.y_val = []
 
     def loadDataset(self):
-        imgs = []
-        xmls = []
+        """Carga las imágenes y las etiquetas del directorio /data y las ordena"""
+        print("Cargando dataset de ", path_dataset)
 
-        imgs = [f for f in glob.glob(os.path.join(self.path_dataset, '*.jpg'))]
-        labels = [f for f in glob.glob(os.path.join(self.path_dataset, '*.txt'))]
-
-        imgs.sort()
-        labels.sort()
-
-        self.imgs = imgs
-        self.labels = labels
-
+        self.imgs = [f for f in glob.glob(os.path.join(self.path_dataset, '*.jpg'))]
+        self.labels = [f for f in glob.glob(os.path.join(self.path_dataset, '*.txt'))]
+        self.imgs.sort()
+        self.labels.sort()
+        print("Dataset cargado")
 
     def splitDataset(self):
         """Divide el dataset cargado en el formato de entrenamiento y validación esperado por YOLO y lo guarda
         en las carpetas correspondientes"""
 
-        X_train, X_val, y_train, y_val = train_test_split(self.imgs, self.labels, test_size=0.20, random_state=42)
+        self.X_train, self.X_val, self.y_train, self.y_val = train_test_split(self.imgs, self.labels, test_size=0.20, random_state=42)
 
-        self.X_train = X_train
-        self.X_val = X_val
-        self.y_train = y_train
-        self.y_val = y_val
+        print("El dataset se ha dividido en entrenamiento y validación")
 
     def saveSplittedDataset(self, trainImgPath, trainLabelPath, valImgPath, valLabelPath):
+        """Guarda las imágenes y anotaciones divididas en entrenamiento y validación en las carpetas correspondientes
+        según el formato esperado por YOLO"""
+
+        print("Guardando las imágenes de train y validación en las carpetas correspondientes")
 
         # Guardar imágenes de train en el directorio correspondiente
         for i in range(len(self.X_train)):
@@ -54,7 +52,7 @@ class trainCell:
             img_name = self.X_train[i].split("\\")[-1]
             label_name = self.y_train[i].split("\\")[-1]
 
-            # Copiar imagen y txt al directorio de dataset correspondiente
+            # Copiar imagen y txt al directorio dataset correspondiente
             shutil.copy(self.X_train[i], os.path.join(trainImgPath, img_name))
             shutil.copy(self.y_train[i], os.path.join(trainLabelPath, label_name))
 
@@ -64,49 +62,66 @@ class trainCell:
             img_name = self.X_val[i].split("\\")[-1]
             label_name = self.y_val[i].split("\\")[-1]
 
-            # Copiar imagen y txt al directorio de dataset correspondiente
+            # Copiar imagen y txt al directorio dataset correspondiente
             shutil.copy(self.X_val[i], os.path.join(valImgPath, img_name))
             shutil.copy(self.y_val[i], os.path.join(valLabelPath, label_name))
 
+        print("Imágenes y anotaciones guardadas")
 
     def trainModel(self, batch=4, epochs=1):
         """Entrena la red neuronal YOLO con el dataset configurado mediante la línea de comandos"""
+        print("Entrenando red YOLO")
+        #cmd = '!python yolov5/train.py --img 640 --batch 4 --epochs 5 --data dataset.yaml --weights yolov5s.pt --cache'
 
-        cmd = "python train.py --img 640 --batch " + str(batch) +" --epochs "+ str(epochs) +" --data dataset.yaml --weights yolov5s.pt --cache"
+        cmd = "python yolov5/train.py --img 640 --batch " + str(batch) +" --epochs "+ str(epochs) +" --data dataset.yaml --weights yolov5s.pt --cache"
         result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
         print(result.stdout)
+        print("Entrenamiento terminado")
 
 
-    def saveModel(self,  path_weight):
+
+    def saveModel(self, path_weight, path_save):
         """Carga el modelo de YOLO y los pesos entrenados y guarda el modelo completo"""
 
-        # Cargar modelo YOLOv5 y los pesos entrenador
         model = torch.hub.load('ultralytics/yolov5', 'custom', path=path_weight, force_reload=True)
 
-        # Definir tamaño de entrada de la imagen
+        # Definir tensor de entrada a la red neuronal
         input_size = (3, 640, 640)
-
-        # Crear tensor de entrada
         dummy_input = torch.randn(1, *input_size)
 
         # Exportar modelo en formato ONNX
         torch.onnx.export(model, dummy_input, "yolov5s_cells.onnx", opset_version=12)
 
+        print("Los pesos ", path_weight, " han sido guardados dentro del modelo ", path_save)
 
 
 
-
-path_dataset = 'C:\\Users\\Usuario\\Desktop\\AIVA_2023-Proyecto-Celulas\\Code\\data\\'
 
 # Directorios para guardar correctamente imágenes de entrenamiento y validación para poder entrenar la red
-trainImgPath = 'C:\\Users\\Usuario\\Desktop\\AIVA_2023-Proyecto-Celulas\\Code\\dataset\\images\\train\\'
-trainLabelPath = 'C:\\Users\\Usuario\\Desktop\\AIVA_2023-Proyecto-Celulas\\Code\\dataset\\labels\\train\\'
+trainImgPath = 'C:\\Users\\Usuario\\Desktop\\AIVA_2023-Proyecto-Celulas\\Code\\trainingCell\\dataset\\images\\train\\'
+trainLabelPath = 'C:\\Users\\Usuario\\Desktop\\AIVA_2023-Proyecto-Celulas\\Code\\trainingCell\\dataset\\labels\\train\\'
 
-valImgPath = 'C:\\Users\\Usuario\\Desktop\\AIVA_2023-Proyecto-Celulas\\Code\\\\images\\val\\'
-valLabelPath = 'C:\\Users\\Usuario\\Desktop\\AIVA_2023-Proyecto-Celulas\\Code\\dataset\\labels\\val\\'
+valImgPath = 'C:\\Users\\Usuario\\Desktop\\AIVA_2023-Proyecto-Celulas\\Code\\trainingCell\\dataset\\images\\val\\'
+valLabelPath = 'C:\\Users\\Usuario\\Desktop\\AIVA_2023-Proyecto-Celulas\\Code\\trainingCell\\dataset\\labels\\val\\'
 
 # Directorio donde se han guardado los pesos
-path_weight = 'C:\\Users\\Usuario\\Desktop\\AIVA_2023-Proyecto-Celulas\\Code\\yolov5\\runs\\train\\exp5\\weights\\best.pt'
+#path_weight = 'C:\\Users\\Usuario\\Desktop\\AIVA_2023-Proyecto-Celulas\\Code\\trainingCell\\yolov5\\runs\\train\\exp5\\weights\\best.pt'
+path_weight = 'C:\\Users\\Usuario\\Desktop\\AIVA_2023-Proyecto-Celulas\\Code\\trainingCell\\best.pt'
+
+# Nombre para guardar el modelo completo entrenado
+path_save = "yolov5s_cells.onnx"
+
+# Directorio donde se encuentran las imágenes y las anotaciones mezcladas
+path_dataset = 'C:\\Users\\Usuario\\Desktop\\AIVA_2023-Proyecto-Celulas\\Code\\trainingCell\\data\\'
+
+# Crear instancia de trainCell
+trainCell1 = trainCell(path_dataset)
+trainCell1.loadDataset()
+trainCell1.splitDataset()
+trainCell1.saveSplittedDataset(trainImgPath, trainLabelPath, valImgPath, valLabelPath)
+trainCell1.trainModel(batch=4,epochs=5)
+#trainCell1.saveModel(path_weight, path_save)
+
 
 
 
